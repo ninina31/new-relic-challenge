78362deea906681afda7566f62d189af628128c6
Maria Andreina Loriente Caiazzo <ninina31@gmail.com>
2019-09-30 00:57:49 +0200

    Add initial files and configuration for Webpack and Babel

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: [ './src/js/index.js' ],
+
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+
+  /*module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [ "style-loader", "css-loader" ]
+      }
+    ]
+  },*/
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 9000,
+    noInfo: true,
+    stats: 'minimal',
+    hot: true,
+    open: true,
+    watchContentBase: true
+  }
+};
